0a06bdfd109684d8970b150c1552b7003661e804
5punk <avaneesh@2die4.com>
2020-05-16 15:24:11 -0700

    add redactor

--- src/config/restaurant.js
+++ src/config/restaurant.js
@@ -1,13 +1,13 @@
 module.exports = {
   // login to grubhub dashboard
   // https://restaurant.grubhub.com/dashboard/${ID}
-  grubhubId: "1226120",
+  grubhubId: "", // prevent-sensitive-commit-string
 
   // login to doordash dashboard
   // https://www.doordash.com/merchant/summary?store_id=${ID}
-  doordashId: "633917",
+  doordashId: "", // prevent-sensitive-commit-string
 
   // login to ubereats dashboard
   // https://restaurant.uber.com/home/${ID}
-  ubereatsId: "742f69d2-d69a-415d-bb99-acfacaa65b66"
+  ubereatsId: "" // prevent-sensitive-commit-string
 };
