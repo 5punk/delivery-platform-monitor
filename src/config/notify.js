4aaf43b8ffd28b7d7d1229728656c2b5589af0ec
5punk <avaneesh@2die4.com>
2020-05-16 15:21:21 -0700

    daemon mode enabled

--- src/config/notify.js
+++ src/config/notify.js
@@ -3,14 +3,10 @@ module.exports = {
   notifEnabled: true,
 
   // send to
-  emailList: [
-    "6177757640@txt.att.net",
-    "7602168094@txt.att.net",
-    "8583820463@tmomail.net"
-  ], // prevent-sensitive-commit-array
+  emailList: [], // prevent-sensitive-commit-array
 
   // send from
-  fromEmail: "info@landonseastmeetswest.com", // prevent-sensitive-commit-string
+  fromEmail: "", // prevent-sensitive-commit-string
 
   // email body template
   serviceDownMessage: service =>
@@ -19,13 +15,13 @@ module.exports = {
   // nodemailer options
   // https://nodemailer.com/usage/
   nodeMailerOpts: {
-    host: "mail.landonseastmeetswest.com", // prevent-sensitive-commit-string
-    name: "landonseastmeetswest.com", // prevent-sensitive-commit-string
+    host: "", // prevent-sensitive-commit-string
+    name: "", // prevent-sensitive-commit-string
     port: 465,
     secure: true,
     auth: {
-      user: "info@landonseastmeetswest.com", // prevent-sensitive-commit-string
-      pass: "@Landons2020" // prevent-sensitive-commit-string
+      user: "", // prevent-sensitive-commit-string
+      pass: "" // prevent-sensitive-commit-string
     }
   }
 };
